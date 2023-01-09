afb12743ec5e16e57a84c522de772054eaad3981
richard200 <richmn200@gmail.com>
2023-01-09 14:00:46 +0300

    Completed Assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,20 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
-
+import MesaVerde from "./parks/MesaVerde";
+// import { trees, wildlife, elevation} from "./parks/RockyMountain.js"
+import * as RockyFunctions from "./parks/RockyMountain.js"
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
+   console.log(RockyFunctions.trees);
+   RockyFunctions.wildlife()
+   RockyFunctions.elevation()
 
-  return <h1>Colorado State Parks!</h1>;
+  return (
+    <div>
+      <MesaVerde />
+  <h1>Colorado State Parks!</h1>
+  </div>
+  )
 }
+
+export default ColoradoStateParks
